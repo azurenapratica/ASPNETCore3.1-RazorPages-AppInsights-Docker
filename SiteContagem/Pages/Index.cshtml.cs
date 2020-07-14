50449a1b1fd6f087e75dc2c4c854cb65bf92e0bd
Renato Groffe <renato.groffe@yahoo.com.br>
2020-07-13 21:12:32 -0300

    ASP.NET Core 3.1 + Razor Pages + Docker + Simulação de Falhas + Azure Application Insights
    ASP.NET Core 3.1 + Razor Pages + Docker + Simulação de Falhas + Azure Application Insights

---
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace SiteContagem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private static readonly Contador _CONTADOR = new Contador();
+
+        public void OnGet([FromServices]ILogger<IndexModel> logger,
+            [FromServices]IConfiguration configuration)
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                logger.LogInformation($"Contador - Valor atual: {_CONTADOR.ValorAtual}");
+
+                if (_CONTADOR.ValorAtual % 4 == 0)
+                    throw new Exception("Simulação de Erro em Index.cshtml.cs...");
+
+                TempData["Contador"] = _CONTADOR.ValorAtual;
+                TempData["Local"] = _CONTADOR.Local;
+                TempData["Kernel"] = _CONTADOR.Kernel;
+                TempData["TargetFramework"] = _CONTADOR.TargetFramework;
+                TempData["MensagemFixa"] = "Teste";
+                TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
+            }            
+        }
+    }
+}
